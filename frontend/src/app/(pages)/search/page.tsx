d2b3f1994617a33fb426cd42b5c741a66e200122
Trong Nguyen <trongnp@gmail.com>
2026-06-14 23:56:24 +0700

    feat(search): responsive result search page for Mobile

--- frontend/src/app/(pages)/search/page.tsx
+++ frontend/src/app/(pages)/search/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
             </select>
           </div>
           <div
-            className="grid grid-cols-3 gap-[20px]"
+            className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-[20px]"
           >
             <ProfileCart />
             <ProfileCart />
